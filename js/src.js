c91f7d023db3ea95eef064e5d4caf9799df5043c
Dawid <dawdud12@gmail.com>
2018-04-04 00:13:39 +0200

    refactor displayError method and  add new addNode method

--- js/src.js
+++ js/src.js
@@ -34,7 +34,7 @@ function demoFromHTML()
 function currentDate()
 {
 var today= new Date();
-var dd= today.getDate;
+var dd= today.getDate();
 var mm= today.getMonth()+1;
 var yyyy= today.getFullYear();
 if(dd<10)
@@ -44,10 +44,11 @@ if(dd<10)
 if(mm<10)
 {
   mm='0'+mm;
-}
-today=dd+'-'+dd+'-'+yyyy;
+
+today=dd+'-'+mm+'-'+yyyy;}
+console.log(today);
 $('.date').datepicker({ dateFormat: 'dd-mm-yy' });
-$('.date').attr('value',today.toString());
+$('.date').attr('value',today);
 }
 
 function Input(el,length)
@@ -55,7 +56,6 @@ function Input(el,length)
     this.el=el;
     this.er=0;
     this.create_node=0;
-    this.elementLength=length;
     this.inputs= document.getElementsByTagName('input');
 
 }
@@ -107,15 +107,10 @@ Input.prototype.displayError= function(name, error)
 
       //TODO this is not finishedS
       name.className+='  ErrorClass';
-      document.getElementById("content").innerHTML="";
+      document.getElementById("#content").innerHTML="";
       var errordiv= document.createElement("div");
       errordiv.classList.add("ErrorDiv");
       errordiv.innerHTML="<h3>"+error+"</h3>";
-
-
-
-
-
 
 
     };
@@ -134,6 +129,53 @@ Input.prototype.removeElement= function()
         }
 
     }
+
+};
+Input.prototype.calcVat=function()
+    {
+        var amount= document.getElementById('amount');
+        var nettoPrice= document.getElementById('netto-price');
+        var nettoWorth= document.getElementById('netto-worth');
+        var amountVat= document.getElementById('amount-vat');
+        var bruttoworth=  document.getElementById('brutto-worth');
+
+
+        nettoPrice.addEventListener('input', function()
+        {
+         var rateVat= document.getElementById('VAT-rate').value.replace('%','');
+         var  parseAmount=parseFloat(amount.value);
+         var  parseNettoPrice= parseFloat(nettoPrice.value);
+         var  parserateVat= parseFloat(rateVat);
+         var  worthNetto= parseAmount* parseNettoPrice;
+
+
+         if(worthNetto>0)
+         {
+             worthNetto= Math.round(worthNetto*100)/100;
+             nettoWorth.value= worthNetto;
+             amountVat.value= (parserateVat*worthNetto)/100;
+             bruttoworth.value= worthNetto + parseFloat(amountVat.value);
+         }
+         else
+         {
+             nettoWorth.value=0;
+         }
+        })
+
+    };
+
+Input.prototype.createTable= function()
+{
+
+    this.tblbtn= document.getElementById('btn-table');
+    this.tblbtn.addEventListener('click', function(){
+
+       var tablediv= document.getElementById('ipt-table');
+       var table=  document.getElementsByTagName('table')[0];
+       var clone= table.cloneNode(true);
+       tablediv.appendChild(clone);
+       console.log(tablediv);
+    });
 
 };
 Input.prototype.display= function()
@@ -165,6 +207,9 @@ var namesv= new Input({name: ['number','Name','Adress',"Postal", "NIP","REGON"],
         "Przykład poprawnego użycia: 00-000","Niepoprawny NIP", "Niepoprawny REGON"],
     regexp:["","","","\\d{2}-\\d{3}","^[0-9]{10}$", "^[0-9]{9}$"]});
 
+namesv.calcVat();
+namesv.createTable();
 namesv.display();
+
 currentDate();
 })(jQuery);
